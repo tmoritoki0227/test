be33112b5d58a5e3fba89c45d3cfd19d2cdf7661
tmoritoki0227 <tmoritoki0227@gmail.com>
2016-09-29 12:57:04 +0000

    first commit

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import run, env, put, sudo
+
+# env.use_ssh_config = True
+
+def uname():
+    run("uname")
+def uname_sudo():
+    sudo("uname")
+
+
+def scp_ex():
+  put('/home/ec2-user/git/test/*', '/home/ec2-user/')
+def scp():
+  put('/var/lib/jenkins/workspace/git_clone/*', '/var/www/html/app')
+
